e335e52c266278783c8ddc67fc5c22cb4c3150b3
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-09 22:19:12 -0300

    feat: add attribute dateNominated

--- src/main/java/Recommendation.java
+++ src/main/java/Recommendation.java
@@ -1,10 +1,26 @@
+import java.time.LocalDateTime;
+
 public class Recommendation {
 
     private Student nominated;
     private Student informed;
+    private LocalDateTime dateNominated;
 
     public Recommendation(Student nominated, Student informed) {
         this.nominated = nominated;
         this.informed = informed;
+        this.dateNominated = LocalDateTime.now();
+    }
+
+    public Student getNominated() {
+        return nominated;
+    }
+
+    public Student getInformed() {
+        return informed;
+    }
+
+    public LocalDateTime getDateNominated() {
+        return dateNominated;
     }
 }
