d02bcbe9f746024987011ea5458c585b0cc0e972
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-10 20:27:12 -0300

    feat: add application layer and recommendation communication

---
+++ src/main/java/application/recommendation/SendMailRecommendation.java
@@ -0,0 +1,9 @@
+package application.recommendation;
+
+import domain.student.Student;
+
+public interface SendMailRecommendation {
+
+    void send(Student studentNominated);
+
+}
---
+++ src/main/java/infrastructure/recommendation/SendMailRecommendationWithJavaMail.java
@@ -0,0 +1,11 @@
+package infrastructure.recommendation;
+
+import application.recommendation.SendMailRecommendation;
+import domain.student.Student;
+
+public class SendMailRecommendationWithJavaMail implements SendMailRecommendation {
+    @Override
+    public void send(Student studentNominated) {
+        // TODO: SEND MAIL LOGIC
+    }
+}
