d1423756d58f75108747875400497490ca584696
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-09 22:59:24 -0300

    chore: add student repository

---
+++ src/main/java/domain/student/StudentRepository.java
@@ -0,0 +1,13 @@
+package domain.student;
+
+import java.util.List;
+
+public interface StudentRepository {
+
+    void register(Student student);
+
+    Student findByCPF(CPF cpf);
+
+    List<Student> findAllStudentsRegistered();
+
+}
