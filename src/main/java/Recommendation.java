336b9a5567a6e6b6f1410c8f94813615ed812b5f
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-09 22:13:40 -0300

    feat: add recommendation class

---
+++ src/main/java/Recommendation.java
@@ -0,0 +1,10 @@
+public class Recommendation {
+
+    private Student nominated;
+    private Student informed;
+
+    public Recommendation(Student nominated, Student informed) {
+        this.nominated = nominated;
+        this.informed = informed;
+    }
+}
