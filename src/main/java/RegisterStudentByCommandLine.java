1f6b172cf3cab16d589fa09678e0cef6eccfddb1
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-10 21:15:35 -0300

    feat: main class to register use

---
+++ src/main/java/RegisterStudentByCommandLine.java
@@ -0,0 +1,22 @@
+import application.student.StudentRegister;
+import application.student.StudentRegisterDto;
+import domain.student.exceptions.CPFException;
+import domain.student.exceptions.EmailException;
+import infrastructure.student.StudentRepositoryInMemory;
+
+public class RegisterStudentByCommandLine {
+
+    public static void main(String[] args) throws EmailException, CPFException {
+
+        var name = "John Doe";
+        var cpf = "01234567890";
+        var email = "johndo@gmail.com";
+
+        var studentRepo = new StudentRepositoryInMemory();
+        var studentService = new StudentRegister(studentRepo);
+        var studentDto = new StudentRegisterDto(name, cpf, email);
+        studentService.execute(studentDto);
+
+    }
+
+}
